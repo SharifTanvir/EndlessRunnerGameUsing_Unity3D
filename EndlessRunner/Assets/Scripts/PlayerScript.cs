4a020d25e722aee4d154e094feab3f48e6b16bb3
Md Shariful Islam <40144211+SharifTanvir@users.noreply.github.com>
2018-09-30 21:21:04 +0600

    Add files via upload

---
+++ EndlessRunner/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed;
+	private Vector3 dir;
+	public GameObject PS;
+	public GameObject ResetButton;
+
+
+	private bool isDead;
+
+
+	// Use this for initialization
+	void Start () {
+		isDead = false;
+		dir = Vector3.forward;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) && !isDead ) {
+			if (dir == Vector3.forward) {	
+				dir = Vector3.left;
+
+			}
+			else {
+				dir = Vector3.forward;
+			}
+
+		}
+
+		float amountMove = speed * Time.deltaTime;
+		transform.Translate (dir * amountMove);
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Pickup") {
+			other.gameObject.SetActive (false);
+			Instantiate (PS, transform.position, Quaternion.identity);
+		}
+
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Tile") {
+			RaycastHit hit;
+			Ray downRay = new Ray (transform.position, -Vector3.up);
+			if(! Physics.Raycast(downRay, out hit)){
+				//kill player
+				isDead=true;
+				ResetButton.SetActive (true);
+
+				if(transform.childCount>0){
+					transform.GetChild (0).transform.parent = null;
+
+				}
+
+
+			}
+		}
+	}
+		
+}
---
+++ EndlessRunner/Assets/Scripts/TileManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+
+	public GameObject[] tilePre;
+	public GameObject currentTile;
+
+	private static TileManager instance; 
+
+	private Stack<GameObject> leftTiles = new Stack<GameObject>();
+	public Stack<GameObject> LeftTiles{
+		get{ return leftTiles;}
+		set{ leftTiles = value;}
+	}
+
+	private Stack<GameObject> topTiles = new Stack<GameObject>();
+	public Stack<GameObject> TopTiles{
+		get{ return topTiles;}
+		set{ topTiles = value;}
+	}
+
+	public static TileManager Instance{
+		get{
+
+			if (instance == null) {
+				instance = GameObject.FindObjectOfType<TileManager> ();
+			}
+
+			return instance; 
+		
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		CreatTiles (20);
+
+		for (int i = 0; i < 10; i++) {
+			
+			SpawnTile ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void CreatTiles(int amount){
+		for (int i = 0; i < amount; i++) {
+
+			leftTiles.Push (Instantiate (tilePre [0]));
+			topTiles.Push (Instantiate (tilePre [1]));
+			topTiles.Peek ().name = "TopTile";
+			topTiles.Peek ().SetActive (false);
+			leftTiles.Peek ().name = "LeftTile";
+			leftTiles.Peek ().SetActive (false);
+		}
+
+	}
+
+	public void SpawnTile(){
+
+
+		if(leftTiles.Count == 0 || topTiles.Count ==0){
+			CreatTiles (10);
+		}
+
+		int randomIndex = Random.Range(0,2);
+
+		if(randomIndex==0){
+			GameObject temp = leftTiles.Pop ();
+			temp.SetActive (true);
+			temp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
+			currentTile = temp;
+		}
+		else if (randomIndex==1){
+			GameObject temp = topTiles.Pop ();
+			temp.SetActive (true);
+			temp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
+			currentTile = temp;
+		}
+		int spawnPickup = Random.Range (0,10);
+		if(spawnPickup==0){
+			currentTile.transform.GetChild (1).gameObject.SetActive (true);
+		}
+
+		//currentTile= (GameObject) Instantiate (tilePre[randomIndex],currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
+	}
+
+	public void ResetGame(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
